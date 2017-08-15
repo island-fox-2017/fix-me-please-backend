d20b71150bf1ba20b14c44f0e2d9ecbae0666ddb
Fickry Bil Iman <fickry@Fickrys-Macbook-Pro.local>
2017-08-15 11:05:09 +0700

    get dan post ok

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(bodyParser.json({type: 'application/*+json'}))
+app.use(bodyParser.json({type: 'application/x-www-form-urlencoded'}))
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
