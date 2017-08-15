e13252358862750839820ecf94657212fe55f2a9
pdvega <pdvegaa@gmail.com>
2017-08-15 10:15:52 +0700

    fixing Router.use() requires middleware function but got a Object

--- routes/transactions.js
+++ routes/transactions.js
@@ -6,3 +6,5 @@ router.get('/', transactionController.all) //fix typo transactionController
 router.post('/', transactionController.create) //fix typo
 router.put('/:id', transactionController.update) //fix typo
 router.delete('/:id', transactionController.delete) // fix typo
+
+module.exports = router; //fixing Router.use() requires middleware function but got a Object
