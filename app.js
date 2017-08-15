777b6a5c469fabda65cd93cae55c151990fc27d5
Patrick Benz <patrickbenz.09@gmail.com>
2017-08-15 10:38:05 +0700

    require body parser and initialize it

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
@@ -8,6 +9,9 @@ mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
 
 var books = require('./routes/books');
 var transactions = require('./routes/transactions');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/books', books);
 app.use('/transactions', transactions);
