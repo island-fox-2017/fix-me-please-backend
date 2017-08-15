e6432fa08124beb050b56a26497cb7b471fce051
Adith <adithwidyapradipta@gmail.com>
2017-08-15 10:36:23 +0700

    debug require  router

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,43 +1,41 @@
 var Transaction = require('../models/Transaction')
 
 module.exports = {
-    all: function(req, res) {
-        Transaction.find(function(err, transactions) {
-            if (err) {
-                res.send({ err: err })
-            }
-            res.send(transactions)
-        })
-    },
-    create: function(req, res) {
-        var transaction = new Transaction(req.body);
-        transaction.save(function(err, result) {
-            if (err) {
-                res.send({ err: err })
-            } else {
+        all: function(req, res) {
+            Transaction.find(function(err, transactions) {
+                if (err) {
+                    res.send({ err: err })
+                }
+                res.send(transactions)
+            })
+        },
+        create: function(req, res) {
+            var transaction = new Transaction(req.body);
+            transaction.save(function(err, result) {
+                if (err) {
+                    res.send({ err: err })
+                } else {
+                    res.send(result)
+                }
                 res.send(result)
-            }
-            res.send(result)
-        });
-    },
-    update: function(req, res) {
-        Transaction.update({ _id: req.id }, {
-            $push: {
-                booklist: req.body
-            }
-        }, function(err, result) {
-            if (err) {
-                res.send({ err: err })
-            }
-            res.send(result)
-        });
-    },
-    delete: function(req, res) {
-        Transaction.remove({ _id: req.id }, function(err, result) {
+            });
+        },
+        update: function(req, res) {
+            Transaction.update({ _id: req.id }, {
+                $set: req.body
+            }, function(err, result) {
                 if (err) {
                     res.send({ err: err })
                 }
                 res.send(result)
-            }
-        });
-}
+            });
+        },
+        delete: function(req, res) {
+            Transaction.remove({ _id: req.id }, function(err, result) {
+                if (err) {
+                    res.send({ err: err })
+                } else {
+                    res.send(result)
+                }
+            });
+        }
