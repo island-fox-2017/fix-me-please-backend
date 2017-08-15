44c60b3422b877c815edd0a1ebe637ff40bde705
budhip <ryo.bunraku@yahoo.com>
2017-08-15 10:38:05 +0700

    fix di models

--- models/Book.js
+++ models/Book.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
-var bookSchema = mongoose.Schema({
+var bookSchema = new Schema({
   isbn: String,
   title: String,
   author: String,
@@ -8,6 +9,7 @@ var bookSchema = mongoose.Schema({
   stock: Number
 });
 
-var Book = mongoose.model('Book', booksSchema);
+//typo booksSchema menjadi bookSchema
+var Book = mongoose.model('Book', bookSchema);
 
 module.exports = Book
