1360bd099534fc13c2181b3be45b1fc9f2fe0bbf
rusli abdul gani <rusli.gani88@gmail.com>
2017-08-15 10:40:46 +0700

    tambahin body-parser

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
 });
 
+var bodyParser = require('body-parser')
 var books = require('./routes/books');
 var transactions = require('./routes/transactions');
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json 
+app.use(bodyParser.json())
 
 app.use('/books', books);
 app.use('/transactions', transactions);
--- models/Book.js
+++ models/Book.js
@@ -4,7 +4,7 @@ var booksSchema = new Schema({
   isbn: String,
   title: String,
   author: String,
-  category: Number,
+  category: String,
   stock: Number
 });
 
