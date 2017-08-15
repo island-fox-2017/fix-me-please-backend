66d2a45f72e84fec93f962a295c6e602eb520b88
pdvega <pdvegaa@gmail.com>
2017-08-15 09:54:27 +0700

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express;
+const bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
